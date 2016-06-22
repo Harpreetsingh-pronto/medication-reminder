557144fd080a16ea9e4d5c26838003bd9b2a79cb
Bryan Higgins <bryan@bryanhiggins.net>
2016-06-22 12:03:37 -0400

    Initial commit

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('medicationReminderApp').controller('MainCtrl', function ($scope, $http, $window) {
+
+    var start = moment().format('MM/DD/YYYY'),
+        end = moment().add(1, 'day').format('MM/DD/YYYY');
+
+    $http.get('/api/medications?start=' + start + '&end=' + end).then(function (meds) {
+        $scope.meds = meds.data;
+    });
+
+    $window.setInterval(function () {
+        $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        $scope.$apply();
+    }, 1000);
+
+});
